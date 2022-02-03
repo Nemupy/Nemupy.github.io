9d449bec934f1e42ca7b77bf5f5281c49503718a
音夢?! <82650472+Nemu627@users.noreply.github.com>
2022-02-03 23:46:38 +0900

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,43 +1,12 @@
-const applyVisited = (status) => {
-  status = "";
-  sessionStorage.setItem("status", status);
+var _switchTheme = () => {
+  var t =
+      localStorage.getItem("theme") === "is-theme-dark"
+          ? "is-theme-light"
+          : "is-theme-dark";
+  localStorage.setItem("theme", t);
+  return t;
 };
-const applyTheme = (themeName) => {
-  sessionStorage.setItem("theme", themeName);
-  document.documentElement.className = themeName;
-};
-const initialTheme = () => {
-  const prefersColorSchemeDark = matchMedia(
-    "(prefers-color-scheme: dark)"
-  ).matches;
-  if (prefersColorSchemeDark) {
-    applyTheme("is-theme-dark");
-  } else {
-    applyTheme("is-theme-light");
-  }
-};
-const getStorageTheme = () => {
-  const storageTheme = sessionStorage.getItem("theme");
-  if (storageTheme === "is-theme-dark") {
-    applyTheme("is-theme-dark");
-  } else if (storageTheme === "is-theme-light") {
-    applyTheme("is-theme-light");
-  }
-};
-const switchTheme = () => {
-  const storageTheme = sessionStorage.getItem("theme");
-  if (storageTheme !== "is-theme-dark") {
-    applyTheme("is-theme-dark");
-  } else {
-    applyTheme("is-theme-light");
-  }
-};
-const discriminationTheme = () => {
-  const getStrageVisited = sessionStorage.getItem("status");
-  if (getStrageVisited) {
-    getStorageTheme();
-  } else {
-    initialTheme();
-    applyVisited("visted");
-  }
-};
+var switchTheme = (t) =>
+  (document.documentElement.className = t ?? _switchTheme());
+window.onload = () =>
+switchTheme(localStorage.getItem("theme") ?? "is-theme-light");
